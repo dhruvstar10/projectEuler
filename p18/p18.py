d14f5f8d91f3469b0d620584533b8bddf3379b98
Dhruv Goel <dhruvstar10@gmail.com>
2012-10-19 16:47:33 -0700

    Creating folder/files for p18

---
+++ p18/p18.py
@@ -0,0 +1,3 @@
+# a program to solve problem 18 of project euler
+# read a pyramid of numbers and find the maximal sum on a path from top to bottom
+
---
+++ p18/readPyramid.py
@@ -0,0 +1,1 @@
+# a util file to read a pyramid/triangle of numbers and convert it to a matrix with 0's above the diagonal
